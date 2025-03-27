19bb70f0985b93161c1cfc6c27002293c66ed9e9
LYDIA <lydiakinuthia91@gmail.com>
2025-03-27 11:18:06 +0300

    removing modules

--- phase-1/flatdango-mock-api/server.js
+++ phase-1/flatdango-mock-api/server.js
@@ -1,12 +1,11 @@
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
 
-const server = jsonServer.create()
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults({ static: 'public' });
 
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('/api', router)
+server.use(middlewares);
+server.use('/api', router);
 server.listen(process.env.PORT || 3000, () => {
-    console.log(`JSON Server is running on port ${process.env.PORT || 3000}`);
-  });
+  console.log('JSON Server is running');
+});
